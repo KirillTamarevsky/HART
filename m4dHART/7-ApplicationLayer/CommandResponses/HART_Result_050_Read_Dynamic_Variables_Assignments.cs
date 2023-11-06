02ea9131d58667a60bdc6638a9f4f3f47588f200
Kirill Tamarevsky <tamarevsky@hotmail.com>
2023-11-06 14:04:24 +0300

    add command 50

---
+++ m4dHART/7-ApplicationLayer/CommandResponses/HART_Result_050_Read_Dynamic_Variables_Assignments.cs
@@ -0,0 +1,20 @@
+﻿using m4dHART._2_DataLinkLayer.Wired_Token_Passing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m4dHART._7_ApplicationLayer.CommandResponses
+{
+    public class HART_Result_050_Read_Dynamic_Variables_Assignments : CommandResponse
+    {
+        public byte DeviceVariable_assigned_to_PV => Data[0];
+        public byte DeviceVariable_assigned_to_SV => Data[1];
+        public byte DeviceVariable_assigned_to_TV => Data[2];
+        public byte DeviceVariable_assigned_to_QV => Data[3];
+        public HART_Result_050_Read_Dynamic_Variables_Assignments(HARTDatagram command) : base(command)
+        {
+        }
+    }
+}
---
+++ m4dHART/7-ApplicationLayer/Commands/HART_050_Read_Dynamic_Variables_Assignments.cs
@@ -0,0 +1,21 @@
+﻿using m4dHART._2_DataLinkLayer.Wired_Token_Passing;
+using m4dHART._7_ApplicationLayer.CommandResponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m4dHART._7_ApplicationLayer.Commands
+{
+    public class HART_050_Read_Dynamic_Variables_Assignments : HARTCommand
+    {
+        public HART_050_Read_Dynamic_Variables_Assignments() : base(050)
+        {
+        }
+        protected override CommandResponse GetCommandResult(HARTDatagram datagram)
+        {
+            return new HART_Result_050_Read_Dynamic_Variables_Assignments(datagram);
+        }
+    }
+}
