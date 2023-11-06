bdbf245295a5b55a01973862f8e64db5f3a0b0f6
Kirill Tamarevsky <tamarevsky@hotmail.com>
2023-11-06 14:05:05 +0300

    some fixes on command Zero versions

--- m4dHART/7-ApplicationLayer/CommandResponses/HART_Result_000_Zero_Command_v6.cs
+++ m4dHART/7-ApplicationLayer/CommandResponses/HART_Result_000_Zero_Command_v6.cs
@@ -9,27 +9,15 @@ using System.Threading.Tasks;
 
 namespace m4dHART._7_ApplicationLayer.CommandResponses
 {
-    public class HART_Result_000_Zero_Command_v6 : CommandResponse, IReadUniqueIdetifierCommand
+    public class HART_Result_000_Zero_Command_v6 : HART_Result_000_Zero_Command_v5
     {
         internal HART_Result_000_Zero_Command_v6(HARTDatagram command) : base(command)
         {
         }
-        public byte[] ExpandedDeviceTypeCode => new byte[] { Data[1], Data[2] };
-        public int MinNumberOfPreamblesRequiredFromMaster => Data[3];
-        public int HARTProtocolMajorRevisionImplementedByThisDevice => Data[4];
-        public int DeviceRevisionLevel => Data[5];
-        public int SoftwareRevisionLevelOfThisDevice => Data[6];
-        public int HardwareRevisionLevelOfTheElectronicsInThisDevice => (Data[7] & 0b11111000) >> 3;
-        public _010_Physical_Signaling_Code PhysicalSignalingCode => (_010_Physical_Signaling_Code)(Data[7] & 0b00000111);
-        public _011_Flag_Assignments Flags => (_011_Flag_Assignments)Data[8];
-        public byte[] DeviceID => new byte[] { Data[9], Data[10], Data[11] };
         public int NumberOfPreablesToBeSentFromThisSlaveToMaster => Data[12];
         public int LastDeviceVariableCode => Data[13];
         public int ConfigurationChangeCounter => BitConverter.ToInt16(new byte[] { Data[14], Data[15] }, 0);
         public int ExtendedFieldDeviceStatus => Data[16];
-        public int DeviceProfile => Data[21];
-
-        public LongAddress LongAddress => new LongAddress(ExpandedDeviceTypeCode.Concat(DeviceID).ToArray());
 
     }
 }
